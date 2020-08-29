eaca9c4d7ba01c742070ab89539baa92a42d7076
Zeeson <olabrazanislam@gmail.com>
2020-08-28 19:46:40 -0700

    feat: fleetController and routes. fleets posts

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const router = express.Router();
 
 const { 
     userLogin, userApplication, getUsers, userLogout
 } = require("../controllers/auth"); 
 
-const router = express.Router();
+const {
+    getFleets, postFleets
+} = require("../controllers/fleetsController")
+
+
 
 // user routes
 router.get('/', getUsers);
 router.post('/signup', userApplication);
 router.post('/signin', userLogin);
 router.get('/logout', userLogout);
+
+// fleet routes
+router.get("/fleets", getFleets)
+router.post("/fleets", postFleets)
+
+
+
+
 
 module.exports = router; 
