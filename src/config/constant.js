af0c6750c1112816bfd037404a29cd1b01e4368d
Zeeson <olabrazanislam@gmail.com>
2020-09-19 07:41:52 -0700

    working on deployment

--- src/config/constant.js
+++ src/config/constant.js
@@ -5,7 +5,7 @@ if(!dotEnvFound) throw new Error("Could not find dotEnv file")
 
 module.exports = {
     port: process.env.PORT || 2020,
-    databaseURI : process.env.DATABASE_URI || "mongodb://localhost/tractive_db", 
+    databaseURI : process.env.MONGODB_URI || "mongodb://localhost/tractive_db", 
     publicKey: process.env.public_key, 
     sessionKey: process.env.SESSIONKEY || 'f3e29360-dd0e-438c-80b8-b33c00ee6d8c'
 }
