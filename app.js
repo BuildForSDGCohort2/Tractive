aec0185d1e0ae54001d14e97dc699ccde124b2a8
Zeeson <olabrazanislam@gmail.com>
2020-10-03 08:17:26 -0700

    working on deployment

--- app.js
+++ app.js
@@ -56,12 +56,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // app.use(express.static('client/build'));
   // app.use(express.static(path.join(__dirname, "client", "build")))
 
-  // app.get('*',(req, res) => {
-  //     res.sendFile(path.join(__dirname,'client','build','index.html'));
-  // })
+  app.get('*',(req, res) => {
+      res.sendFile(path.join(__dirname,'client','build','index.html'));
+  })
 }
 
 // cors
