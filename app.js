da241c3d205916df9418702a42b951bfa35aaf68
Zeeson <olabrazanislam@gmail.com>
2020-10-11 21:14:51 -0700

    working on heroku get error

--- app.js
+++ app.js
@@ -72,16 +72,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   })
 // }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-}
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build', "index.html"));
-// }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build', "index.html"));
+}
 
 // cors
 app.use(cors()); 
