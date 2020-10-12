28cb7004713435c45b628d982b0b08111b81e6de
Zeeson <olabrazanislam@gmail.com>
2020-10-11 21:59:44 -0700

    working on heroku get error

--- app.js
+++ app.js
@@ -75,8 +75,14 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "client", "build")))
+  // app.use(express.static(path.join(__dirname, "client", "build")))
   // app.use(express.static('client/build', "index.html"));
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
 }
 
 // cors
