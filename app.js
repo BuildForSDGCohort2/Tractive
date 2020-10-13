f26165421b9c2c5356b344db59524c705a44d3ab
Zeeson <olabrazanislam@gmail.com>
2020-10-13 14:48:37 -0700

    production postbuild

--- app.js
+++ app.js
@@ -64,12 +64,13 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  // app.use(express.static(path.join(__dirname, "client", "build")))
+  app.use(express.static('./client/source/app.js'));
+  // app.use(express.static('client/build'));
+  // // app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get('*',(req, res) => {
-      res.sendFile(path.join(__dirname,'client','build'));
-  })
+  // app.get('*',(req, res) => {
+  //     res.sendFile(path.join(__dirname,'client','build'));
+  // })
 }
 
 
