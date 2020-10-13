c7f5ff3ed449c120c88825124c7f35eb69b8c74b
Zeeson <olabrazanislam@gmail.com>
2020-10-13 14:29:56 -0700

    production postbuild

--- app.js
+++ app.js
@@ -63,26 +63,26 @@ app.use(express.json());
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-  // app.use(express.static(path.join(__dirname, "client", "build")))
-
-//   app.get('*',(req, res) => {
-//       res.sendFile(path.join(__dirname,'client','build','index.html'));
-//   })
-// }
-
-
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   // app.use(express.static(path.join(__dirname, "client", "build")))
-  // app.use(express.static('client/build', "index.html"));
-  // app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build'))
-  });
+  app.get('*',(req, res) => {
+      res.sendFile(path.join(__dirname,'client','build'));
+  })
 }
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   // app.use(express.static(path.join(__dirname, "client", "build")))
+//   // app.use(express.static('client/build', "index.html"));
+//   // app.use(express.static(path.join(__dirname, 'client', 'build')))
+
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build'))
+//   });
+// }
 
 // cors
 app.use(cors()); 
