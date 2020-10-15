e64c54b2b0d1adb7829870a6f6e66fe956cabe71
Zeeson <olabrazanislam@gmail.com>
2020-10-15 08:10:29 -0700

    debugging build

--- app.js
+++ app.js
@@ -61,30 +61,18 @@ app.use(express.json());
 // i have spent more than 1 week debugging it
 // coming back to make it work properly InsaAllah
 
-
-
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static('./client/source/app.js'));
   // app.use(express.static('client/build'));
   app.use(express.static(path.join(__dirname, "client", "build"))); 
 
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
 
-
-// if (process.env.NODE_ENV === 'production') {
-//   // app.use(express.static(path.join(__dirname, "client", "build")))
-//   // app.use(express.static('client/build'));
-//   // app.use(express.static(path.join(__dirname, 'client', 'build')))
-
-
-//   // app.get('*', (req, res) => {
-//   //   res.sendFile(path.join(__dirname, 'client', 'build'))
-//   // });
-// }
 
 // cors
 app.use(cors()); 
