c03f9839af4e99c51c71928daba44010f3a3f8b0
Ibrahim Zainab Saliman <45487954+Zeeson@users.noreply.github.com>
2020-12-12 16:44:02 +0100

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -46,7 +46,7 @@ export default class MainNavbar extends Component {
                 </li>
               <li className="nav-item">
                     <NavLink
-                        className="nav-link text-light font-weight-bold" activeClassName="is-active" to="/fleets">Fleets
+                        className="nav-link text-light font-weight-bold" activeClassName="is-active" to="/fleets">Market Place
                         <span class="sr-only">(current)</span>
                     </NavLink>
                 </li>
