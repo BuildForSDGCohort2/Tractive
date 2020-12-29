36e96c47299cc233801aa813b3ca0548e241b1c2
zeeson <olabrazanislam@gmail.com>
2020-12-29 23:48:44 +0100

    resolving conflict

--- client/src/farmer/FarmerRegister.js
+++ client/src/farmer/FarmerRegister.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD:client/src/components/FarmerRegister.js
-
-=======
->>>>>>> c03f9839af4e99c51c71928daba44010f3a3f8b0:client/src/farmer/FarmerRegister.js
 import React, { Component } from 'react';
 // import Button from 'react-bootstrap/Button';
 import { NavLink } from "react-router-dom";
@@ -28,13 +24,13 @@ export default class FarmerRegister extends Component {
             farmAddress: "",
             crops: "",
             password: "",
-            image: null, 
+            image: null,
             messageFromServerFarmer: '',
             showError: false,
             registerError: false,
             loginError: false,
             showMessage: false,
-        
+
         };
     }
 
@@ -52,7 +48,7 @@ export default class FarmerRegister extends Component {
           })
         })
        }
-    
+
     handleChange = farmer => (event) => {
         this.setState({
           [farmer]: event.target.value,
@@ -60,10 +56,10 @@ export default class FarmerRegister extends Component {
       };
 
     registerFarmer = async (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
         const {
             title, address, town, state, farmSize, farmAddress, crops, fullName, gender, email, phone,  country, image, password,
-        } = this.state; 
+        } = this.state;
         const theState = {
             title, address, town, state, farmSize, farmAddress, crops, fullName, gender, email, phone,  country, image, password,
         }
@@ -89,7 +85,7 @@ export default class FarmerRegister extends Component {
                       {
                         title, address, town, state, farmSize, farmAddress, crops, fullName, gender, email, phone,  country, image, password
                       },
-                    );                  
+                    );
                     if(response.data.message){
                          this.setState({
                             showMessage: toast.success("SUCCESS", {
@@ -99,7 +95,7 @@ export default class FarmerRegister extends Component {
                             showError: false,
                             loginError: false,
                             registerError: false,
-    
+
                         });
                     } else{
                         this.setState({
@@ -110,7 +106,7 @@ export default class FarmerRegister extends Component {
                             showError: false,
                             loginError: false,
                             registerError: false,
-    
+
                           });
                     }
                     window.location = '/login-farmer';
@@ -125,8 +121,8 @@ export default class FarmerRegister extends Component {
         }
     }
   }
-    
-} 
+
+}
 
     render() {
       const {
@@ -143,7 +139,7 @@ export default class FarmerRegister extends Component {
         farmAddress,
         crops,
         password,
-        image, 
+        image,
         messageFromServerFarmer,
         showError,
         registerError,
@@ -164,7 +160,7 @@ export default class FarmerRegister extends Component {
                             <button className="btn btn-success btn-large joinbtn">Farmers</button>
                     {/* </NavLink>  */}
                     <form className="m-4" id="contact-form" onSubmit={this.registerFarmer} >
-                                <select required className='form-group form-control' onChange={this.handleChange("title")} value={title} > 
+                                <select required className='form-group form-control' onChange={this.handleChange("title")} value={title} >
                                     <option>Select Title</option>
                                     <option>Mr.</option>
                                     <option>Mrs.</option>
@@ -182,7 +178,7 @@ export default class FarmerRegister extends Component {
                                 <div className="form-group">
                                     <input type="email" className="form-control" onChange={this.handleChange("email")} value={email} placeholder="Email Address" />
                                 </div>
-                                <select required className='form-group form-control' onChange={this.handleChange("gender")} value={gender} > 
+                                <select required className='form-group form-control' onChange={this.handleChange("gender")} value={gender} >
                                     <option>Select Gender</option>
                                     <option>male</option>
                                     <option>female</option>
@@ -220,8 +216,8 @@ export default class FarmerRegister extends Component {
                                     <input className="form-control" type="file" name="image" onChange={this.handleUpload} />
                                  </div>
                                 <div>
-                                    <input className="mr-2" type="checkbox" name="checkbox" value="check" id="agree" /> 
-                                    I have read and agree to the <span className="text-success" > <a className="text-success" href="/terms-and-conditions"> Terms and Conditions</a> 
+                                    <input className="mr-2" type="checkbox" name="checkbox" value="check" id="agree" />
+                                    I have read and agree to the <span className="text-success" > <a className="text-success" href="/terms-and-conditions"> Terms and Conditions</a>
                                     </span> and <span className="text-success"  href="/terms-and-conditions"> <a className="text-success" href="/terms-and-conditions">  Privacy Policy</a> </span>
                                 </div>
                                 <div>
@@ -247,24 +243,24 @@ export default class FarmerRegister extends Component {
                             </div>
                         )}
                     </div>
-                                
+
                     <div className="col-sm-12 col-md-12 col-lg-12 col-xl-4">
                     <NavLink to="/owner-register">
                             <button className="btn btn-success btn-large fleetjoinbtn">Fleet Owners</button>
-                    </NavLink> 
+                    </NavLink>
                     <img className="m-3" src="/images/tractyJoin.png" alt="" />
                     </div>
                     <div className="col-sm-12 col-md-12 col-lg-12 col-xl-4">
                     <NavLink to="/agent-register">
                             <button className="btn btn-success btn-large joinbtn">Agents</button>
-                    </NavLink> 
+                    </NavLink>
                             <img className="m-3" src="/images/youthJoin.png" alt="" />
                     </div>
 
                 </div>
                 <Footer />
                 </div>
-            
+
              );
         }
         // if (messageFromServerFarmer === 'user created') {
@@ -275,9 +271,9 @@ export default class FarmerRegister extends Component {
         //                  <h3>User successfully registered!</h3>
         //                 <button className="ml-2 h2 text-success">Login</button>
         //           </NavLink>
-              
+
         //       </div>
             // );
         // }
-    }   
+    }
 }
